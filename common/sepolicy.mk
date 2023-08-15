6e510b68c0a9a01e123f97fcd2b42fc2e2e67b83
someone5678 <nemui3353@gmail.com>
2023-08-15 16:45:06 +0900

    sepolicy: Initial sepolicy makefile
    Change-Id: I09a45d924059076032fdc3db6e6c93139198ee38

---
+++ common/sepolicy.mk
@@ -0,0 +1,27 @@
+#
+# This policy configuration will be used by all products that
+# inherit from TheParasiteProject
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/common/public
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/common/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/common/dynamic \
+    device/parasite/sepolicy/common/system
+
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/parasite/sepolicy/common/dynamic \
+    device/parasite/sepolicy/common/vendor
+endif
